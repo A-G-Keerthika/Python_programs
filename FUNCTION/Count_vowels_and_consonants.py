ed2889173a0c167d39f213f7ebace4c424d77d9d
A-G-Keerthika <102655306+A-G-Keerthika@users.noreply.github.com>
2022-04-02 11:05:16 +0530

    Create Count_vowels_and_consonants.py

---
+++ FUNCTION/Count_vowels_and_consonants.py
@@ -0,0 +1,16 @@
+#2.	Write a Python function which counts vowels and consonant in a word.
+
+def count(s):
+  v=0
+  c=0
+  for i in s:
+    if((i=='a') or (i=='e') or (i=='i') or (i=='o') or (i=='u') or(i=='A') or (i=='E') or (i=='I') or(i=='O') or (i=='U')):
+      v=v+1
+    else:
+      c=c+1
+  print("\n-----Count of vowels and consonents-----")
+  print("number of vowels : ",v)
+  print("number of consonents : ",c)
+
+s=str(input("enter a string to count vowels and consonants in it : "))
+count(s)
