06b20190c14b845cf8650717499bc9caaee52e69
A-G-Keerthika <102655306+A-G-Keerthika@users.noreply.github.com>
2022-04-02 11:08:39 +0530

    Create Count_positive_and_negative_numbers.py

---
+++ FUNCTION/Count_positive_and_negative_numbers.py
@@ -0,0 +1,20 @@
+#program3 : Write a Python function which counts positive and negative numbers in a given numbers.
+def pncount(n,ls):
+  n=0;
+  p=0;
+  for i in ls:
+    if(i<0):
+      n=n+1
+    else:
+      p=p+1
+  print("\n-----Count of positive and negative numbers-----")
+  print("number of positive numbers : ",p)
+  print("number of negative numbers : ",n)
+
+ls=[]
+n=int(input("enter number of elements in a list : "))
+print("enter the elements of list : ")
+for i in range(0,n):
+  elemt=int(input())
+  ls.append(elemt)
+pncount(n,ls)
